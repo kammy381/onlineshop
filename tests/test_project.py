d7633a3db18614e8844eaf6e97c85e185330df21
kammy <kamilromasz@hotmail.com>
2023-02-10 17:54:46 +0100

    added login and logout test

--- tests/test_project.py
+++ tests/test_project.py
@@ -13,3 +13,22 @@ def test_register(client, app):
 
         assert Users.query.count() == 1
         assert Users.query.first().email == "testuser@hotmail.com"
+
+def test_register_login_logout(client, app):
+    #register
+    client.post("/users/new", data=dict([('username', 'testuser'), ('email', 'testuser@hotmail.com'), ('password_hash', '123'), ('password_hash2', '123'), ('address', '123'), ('postal_code', '123'), ('city', '123'), ('country', '123')]) )
+    #login
+    response=client.post("/login", data=dict([('username', 'testuser'), ('password', '123')]), follow_redirects=True)
+
+    #check if user in db and it it's the correct one
+    with app.app_context():
+        assert Users.query.count() == 1
+        assert Users.query.first().email == "testuser@hotmail.com"
+    #check if user is redirected to dashboard after loging in
+    assert b"<h1>Dashboard</h1>" in response.data
+
+    #logout
+    resp2=client.get("/logout", follow_redirects=True)
+
+    #check if user redirected to indexpage after logging out
+    assert b"<title>Index</title>" in resp2.data
